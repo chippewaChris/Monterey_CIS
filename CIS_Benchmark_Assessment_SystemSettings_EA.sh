53783aec3b455d6f86698957436de56e0709006d
chippewaChris <m.christopher.cohoon@me.com>
2023-04-06 09:25:53 -0500

    Creation of EAs to assess the generated report in a more granular manner.

---
+++ CIS_Benchmark_Assessment_SystemSettings_EA.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+###############################################################################
+#                                                                             
+# -                  CIS Benchmark Assessment System Settings EA                     
+#                                                                             
+# Copyright (C) Thursday, March 2, 2023, Chippewa Limited Liability Co.                                       
+#                                                                             
+# This script is the property of Chippewa Limited Liability Co. and is intended for internal     
+# use only. This script may not be distributed, reproduced, or used in any    
+# manner without the expressed written consent of Chippewa Limited Liability Co..                
+#                                                                             
+# This script is provided "AS IS" and WITHOUT WARRANTY OF ANY KIND,           
+# EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED     
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.         
+#                                                                                                         
+###############################################################################
+file=$(ls -t /Library/Application\ Support/Security\ Audit | head -n 1)
+log="/Library/Application Support/Security Audit/$file"
+fail=$(grep ",2." "$log" | grep -c "FAIL" )
+threshold=1 #user modifiable to allow for leniancy 
+
+if [ $fail -ge $threshold ]; then
+	echo "<result>FAIL</result>"
+else
+	echo "<result>PASS</result>"
+fi
